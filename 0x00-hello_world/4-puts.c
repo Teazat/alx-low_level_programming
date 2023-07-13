46c36bdc5dfa86474470ccba98e9610a7bf392fc
Teazat <zac.thabang@gmail.com>
2023-07-13 10:48:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stduio.h>
+#include <stdio.h>
 /**
  * main - Program that prints a line with puts function
  * Return: 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
