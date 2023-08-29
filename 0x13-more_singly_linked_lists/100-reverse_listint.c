bdf21e4113d33f0c18b724eae70133642de63728
Teazat <zac.thabang@gmail.com>
2023-08-29 16:32:52 -0700

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,19 +8,12 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next;
+	listint_t *prev = NULL, *current = *head, *next;
 
-	while (current)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
+	for (; current != NULL; next = current->next,
+		current->next = prev, prev = current, current = next)
+		;
 
 	*head = prev;
-
 	return (*head);
 }
