da360d526e1ef0c708ed2c67b555e84d342add1d
Teazat <zac.thabang@gmail.com>
2023-12-18 13:21:41 -0800

    file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Read text file and print to STDOUT
+ * @filename: text file to be read
+ * @letters: number of letters to read
+ *
+ * Return: num of bytes read & printed, or 0 on failure or if filename is NULL
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t fd, read_bytes, written_bytes;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	read_bytes = read(fd, buffer, letters);
+	if (read_bytes == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	written_bytes = write(STDOUT_FILENO, buffer, read_bytes);
+	if (written_bytes == -1 || written_bytes != read_bytes)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+	return (written_bytes);
+}
